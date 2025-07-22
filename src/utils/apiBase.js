f668ca230a616db3ad7db2e40d9725613c1ecd55
JackMocho <196417405+JackMocho@users.noreply.github.com>
2025-07-22 23:59:19 +0300

    Connecting backend to frontend

--- src/utils/apiBase.js
+++ src/utils/apiBase.js
@@ -1,3 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://backend-96ih.onrender.com'
-  : 'http://localhost:3000';
+export const API_URL = import.meta.env.VITE_API_URL;
